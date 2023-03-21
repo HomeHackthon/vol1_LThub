9b9913a30607fa13c504f30479f5c4b3f1f6ca89
smaru1111 <96244711+smaru1111@users.noreply.github.com>
2023-03-21 23:54:18 +0900

    add Tailwind導入

---
+++ lthub/tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+module.exports = {
+  mode: 'jit',
+  darkMode: false, // 'media' or 'class'
+  purge: {
+    content: [
+      './src/app/pages/**/*.{js,ts,jsx,tsx}',
+      './src/app/components/**/*.{js,ts,jsx,tsx}'
+    ],
+    options: {
+      // https://purgecss.com/safelisting.html#patterns
+      safelist: {
+        standard: [/^bg-/, /^text-/],
+      },
+    },
+  },
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
